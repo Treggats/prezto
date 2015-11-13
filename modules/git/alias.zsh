c7e7f993ccb9c3ce09458c4679ccacdae8a200b3
Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>
2015-11-13 15:11:12 +0100

    updated gfr alias to look at the current branch
    Signed-off-by: Tonko Mulder <mulder@goldmund-wyldebeast-wunderliebe.com>

--- modules/git/alias.zsh
+++ modules/git/alias.zsh
@@ -78,7 +78,7 @@ alias gdi='git status --porcelain --short --ignored | sed -n "s/^!! //p"'
 alias gf='git fetch'
 alias gfc='git clone'
 alias gfm='git pull'
-alias gfr='git pull --rebase'
+alias gfr='git pull --rebase origin "$(git-branch-current 2> /dev/null)"'
 
 # Grep (g)
 alias gg='git grep'
